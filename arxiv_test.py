abfc806b146cd61390447614398de56a4998b72a
nitto05 <satyammohapatra2005@gmail.com>
2026-06-24 01:24:35 +0530

    test_files and tools separated...

--- arxiv_test.py
+++
@@ -1,5 +0,0 @@
-from tools.arxiv_tool import get_papers
-
-papers = get_papers("physics")
-
-print(papers[:2000])
--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-from tools.poetrydb_tool import get_poems
-
-query = "nature"
-
-poem_data = get_poems(query)
-
-print(poem_data)
